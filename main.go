24e9c2fce0616648c6c351b592a1cd6e014c0207
Sagar Kapasi <SagarKapasi099@gmail.com>
2020-01-08 23:16:34 +0530

    Made \"Home\" Golang Template From index.html

--- main.go
+++ main.go
@@ -2,13 +2,33 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("static")))
+var templatesPath = "templates/*.html"
+var tpl *template.Template
 
-	log.Fatal(http.ListenAndServe(":8080", mux))
+func main() {
+	var err error
+	tpl, err = template.ParseGlob(templatesPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := mux.NewRouter()
+	//mux.Handle("/", http.FileServer(http.Dir("static")))
+	r.HandleFunc("/", HomeHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	err := tpl.ExecuteTemplate(w, "home", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
