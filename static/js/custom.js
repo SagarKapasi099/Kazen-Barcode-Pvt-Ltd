c65097265f68f7dbe61b618bd99a8a3c5521ee60
Sagar Kapasi <SagarKapasi099@gmail.com>
2020-01-13 03:22:27 +0530

    Added localStorage Support For Selected Buttons

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,9 +1,17 @@
 const buttons = document.getElementsByClassName("button");
 
 const buttonClickFunction = function () {
+    let products = JSON.parse(localStorage.getItem("products"));
+    if (products == null) {
+        products = [];
+    }
     if (this.classList.contains('success')) {
+        products = products.filter(e => e !== this.getAttribute("data-id"));
+        localStorage.setItem("products", JSON.stringify(products));
         this.classList.remove('success');
     } else {
+        products.push(this.getAttribute("data-id"));
+        localStorage.setItem("products", JSON.stringify(products));
         this.classList.add('success');
     }
 };
@@ -11,3 +19,16 @@ const buttonClickFunction = function () {
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', buttonClickFunction, false);
 }
+
+// Select All The Products From The Local Storage
+window.onload = function () {
+    let products = JSON.parse(localStorage.getItem("products"));
+    if (products != null) {
+        const productButtons = document.querySelectorAll('[data-id]');
+        for (let i = 0; i < productButtons.length; ++i) {
+            if (products.includes(productButtons[i].getAttribute("data-id"))) {
+                productButtons[i].classList.add('success');
+            }
+        }
+    }
+};
