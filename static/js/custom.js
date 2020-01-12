e0cc5a1134be14a2ea42c1bd18ea78cc4cc84671
Sagar Kapasi <SagarKapasi099@gmail.com>
2020-01-13 02:20:28 +0530

    Added Interested? Button For Products

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,13 @@
-$(".size").on('click', function () {
-    $(this).toggleClass('focus').siblings().removeClass('focus');
-})
+const buttons = document.getElementsByClassName("button");
+
+const buttonClickFunction = function () {
+    if (this.classList.contains('success')) {
+        this.classList.remove('success');
+    } else {
+        this.classList.add('success');
+    }
+};
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', buttonClickFunction, false);
+}
