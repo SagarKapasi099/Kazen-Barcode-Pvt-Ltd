e1cfbd74397b5fb1268a6b6af3953a3150b84d11
Sagar Kapasi <SagarKapasi099@gmail.com>
2020-01-19 20:10:50 +0530

    Added Contact Us With OTP Verification

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"html/template"
 	"log"
@@ -18,6 +19,14 @@ type Product struct {
 	Properties []string
 }
 
+type Enquiry struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Mobile   string `json:"mobile"`
+	Comments string `json:"comments"`
+	OTP      string `json:"otp"`
+}
+
 func main() {
 
 	var err error
@@ -27,9 +36,12 @@ func main() {
 	}
 	r := mux.NewRouter()
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	r.HandleFunc("/", HomeHandler)
-	r.HandleFunc("/about", AboutHandler)
-
+	r.HandleFunc("/", HomeHandler).Methods("GET")
+	r.HandleFunc("/about", AboutHandler).Methods("GET")
+	r.HandleFunc("/contact", ContactUsHandler).Methods("GET")
+	r.HandleFunc("/enquiry", EnquiryHandler).Methods("POST")
+	r.HandleFunc("/verifyOTP", VerifyOTPHandler).Methods("POST")
+	r.HandleFunc("/products", ShowProductsHandler).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
@@ -126,4 +138,91 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
+}
+
+func ContactUsHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	err := tpl.ExecuteTemplate(w, "contact", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func EnquiryHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	number := r.FormValue("number")
+	comments := r.FormValue("comments")
+
+	// TODO: generate OTP
+	otp := "2995"
+
+	currentEnquiry := Enquiry{name, email, number, comments, otp}
+	fmt.Println(currentEnquiry)
+
+	// TODO: save to database
+	customerId := "cust-1" // customer id from database
+
+	jsonResponse := `{
+		"success": true,
+		"message":{
+			"superText":"Please Enter OTP Sent On `+number+`",
+			"subText": "",
+			"buttonText": "Verify"
+		},
+		"number": "`+number+`",
+		"customerId": "`+ customerId +`",
+		"url": "/verifyOTPHandler",
+		"method": "post"
+	}`
+
+	_, err := fmt.Fprintf(w, jsonResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func VerifyOTPHandler(w http.ResponseWriter, r *http.Request) {
+	customerId := r.FormValue("customerId")
+	// TODO get number from id and get OTP from id
+	otp := "399"
+	otpReceived := r.FormValue("otp")
+	fmt.Println(otpReceived)
+	// TODO: avoid brute force attack
+	// TODO: check database for OTP
+	jsonResponse := ``
+	if otpReceived == otp {
+		// OTP matches
+		jsonResponse = `{
+			"success": true,
+			"message": {
+				"superText": "Thank you for your enquiry.",
+				"subText": "It has been forwarded to the relevant department and will be dealt with as soon as possible.",
+				"buttonText": "Show Products With Prices"
+			},
+			"customerId": "`+customerId+`",
+			"url": "/showProducts",
+			"method": "post"
+		}`
+	} else {
+		// OTP does not match
+		jsonResponse = `{
+			"success": false,
+			"message": {
+				"superText": "Something Went Wrong.",
+				"subText": ""
+			},
+			"customerId": "`+customerId+`"
+		}`
+	}
+	_, err := fmt.Fprintf(w, jsonResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ShowProductsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, r.FormValue("customerId"))
+	// TODO extract a view from products grid and show the template here
+	fmt.Fprintf(w, "here are your products")
 }
--- static/js/custom.js
+++ static/js/custom.js
@@ -44,3 +44,231 @@ function updateCount(number) {
         countElement.innerHTML = "0";
     }
 }
+
+/*
+ * Handle Enquiry
+ */
+document.getElementById("enquirySubmitBtn").addEventListener('click', function (e) {
+    e.preventDefault();
+    // debugger;
+    /*!
+  * Serialize all form data into an array
+  * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+  * @param  {Node}   form The form to serialize
+  * @return {String}      The serialized form data
+  */
+    let form = document.getElementById('enquiryForm');
+
+    // Setup our serialized data
+    let formData = new FormData();
+
+
+    // Loop through each field in the form
+    for (let i = 0; i < form.elements.length; i++) {
+
+        let field = form.elements[i];
+
+        // Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+        if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
+
+        // If a multi-select, get all selections
+        if (field.type === 'select-multiple') {
+            for (let n = 0; n < field.options.length; n++) {
+                if (!field.options[n].selected) continue;
+                formData.append(field.name, field.options[n].value);
+            }
+        }
+
+        // Convert field data to a query string
+        else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+            formData.append(field.name, field.value);
+        }
+    }
+
+    console.log(formData);
+
+    const XHR = new XMLHttpRequest();
+
+    // Define what happens on successful data submission
+    XHR.addEventListener('load', function (event) {
+        
+        let jsonResponse = JSON.parse(event.target.response);
+        document.getElementById("superText").innerText = jsonResponse.message.superText;
+        document.getElementById("subText").innerText = jsonResponse.message.subText;
+
+        document.getElementById("enquiryForm").style.display = "none";
+
+        let otpForm = document.createElement("form");
+        otpForm.setAttribute("name", "otpForm");
+        otpForm.setAttribute("id", "otpForm");
+        otpForm.setAttribute("class", "otpForm");
+        otpForm.setAttribute("autocomplete", "off");
+
+        let linebreak = document.createElement("br");
+
+        let otpInput = document.createElement("input");
+        otpInput.setAttribute("name", "otp");
+        otpInput.setAttribute("name", "otp");
+        otpInput.setAttribute("id", "otp");
+        otpInput.setAttribute("class", "h-full-width h-remove-bottom");
+        otpInput.setAttribute("placeholder", "Please Enter OTP for " + jsonResponse.number);
+        otpInput.setAttribute("type", "email");
+
+        let otpInputDiv = document.createElement("div");
+        otpInputDiv.setAttribute("class", "form-field");
+        otpInputDiv.appendChild(otpInput);
+
+        let customerId = document.createElement("input");
+        customerId.style.display = "none";
+        customerId.setAttribute("name", "customerId");
+        customerId.setAttribute("id", "customerId");
+        customerId.setAttribute("value", jsonResponse.customerId);
+
+        let otpSubmitButton = document.createElement("input");
+        otpSubmitButton.setAttribute("name", "otpSubmitBtn");
+        otpSubmitButton.setAttribute("id", "otpSubmitBtn");
+        otpSubmitButton.setAttribute("value", "Submit OTP");
+        otpSubmitButton.setAttribute("class", "btn btn--primary btn--large h-full-width");
+        otpSubmitButton.setAttribute("type", "submit");
+
+        let otpResendButton = document.createElement("input");
+        otpResendButton.setAttribute("onclick", "resendOTP(" + jsonResponse.number + ")");
+        otpResendButton.setAttribute("class", "btn btn--primary btn--large h-full-width");
+        otpResendButton.setAttribute("type", "submit");
+        otpResendButton.setAttribute("id", "otpResendBtn");
+        otpResendButton.setAttribute("value", "Resend OTP");
+
+        document.getElementById("formDiv").appendChild(otpForm);
+
+        otpForm.append(otpInputDiv,
+            document.createElement("br"),
+            customerId,
+            otpSubmitButton,
+            document.createElement("br"),
+            otpResendButton
+        );
+
+        otpForm.scrollIntoView(false);
+    });
+
+    // Define what happens in case of error
+    XHR.addEventListener(' error', function (event) {
+        alert("Please Check Internet And Try Again.");
+        // alert( 'Oops! Something went wrong.' );
+    });
+
+    try {
+        XHR.onreadystatechange = function() {
+            console.log(XHR);
+            if (XHR.readyState == 4 && XHR.status == 0) {
+                alert("Please Check Internet Connection and Try Again");
+            }
+        };
+
+        // Set up our request
+        XHR.open('POST', '/enquiry', false);
+
+        // Send our FormData object; HTTP headers are set automatically
+        XHR.send(formData);
+    } catch (e) {
+        alert("Please Check Internet Connection And Try Again.");
+    }
+
+});
+
+document.addEventListener('click',function(e){
+    if (e.target && e.target.id === "otpSubmitBtn") {
+        e.preventDefault();
+
+        let otp = document.getElementById("otp");
+
+        let otpValue = otp.value;
+        if (otpValue === "" || otpValue == null) {
+            alert("Please Enter Valid OTP");
+            return;
+        }
+
+        let customerId = document.getElementById("customerId");
+        let customerIdValue = customerId.value;
+
+        // Setup our serialized data
+        let formData = new FormData();
+        formData.append("otp", otpValue);
+        formData.append("customerId", customerIdValue);
+
+        const XHR = new XMLHttpRequest();
+
+        // Define what happens on successful data submission
+        XHR.addEventListener('load', function (event) {
+
+            let jsonResponse = JSON.parse(event.target.response);
+
+            if (jsonResponse.success !== true) {
+                alert("OTP Did Not Match.\nPlease Try Again.");
+            } else {
+
+                document.getElementById("otpForm").style.display = "none";
+
+                document.getElementById("superText").innerText = jsonResponse.message.superText;
+                document.getElementById("subText").innerText = jsonResponse.message.subText;
+
+                try {
+                    document.getElementById("productsForm").remove();
+                } catch(e) {
+
+                }
+
+                let productsForm = document.createElement('form');
+                productsForm.setAttribute("id", "productsForm");
+
+                formData.append("customerId", jsonResponse.customerId);
+
+                productsForm.append(customerId);
+                productsForm.method = "post";
+                productsForm.action = "/products";
+                document.body.appendChild(productsForm);
+
+                let showProductsButton = document.createElement("input");
+                showProductsButton.setAttribute("name", "showProducts");
+                showProductsButton.setAttribute("id", "showProducts");
+                showProductsButton.setAttribute("value", "SHOW PRODUCTS WITH PRICE");
+                showProductsButton.setAttribute("class", "btn btn--primary btn--large h-full-width");
+                showProductsButton.setAttribute("type", "submit");
+
+                document.getElementById("formDiv").appendChild(showProductsButton)
+
+
+                // alert( 'Yeah! Data sent and response loaded.' );
+            }
+        });
+
+        // Define what happens in case of error
+        XHR.addEventListener(' error', function (event) {
+            alert("Please Check Internet Connection And Try Again.")
+        });
+
+        try {
+            XHR.onreadystatechange = function() {
+                console.log(XHR);
+                if (XHR.readyState == 4 && XHR.status == 0) {
+                    alert("Please Check Internet Connection and Try Again");
+                }
+            };
+
+            // Set up our request
+            XHR.open('POST', '/verifyOTP', false);
+
+            // Send our FormData object; HTTP headers are set automatically
+            XHR.send(formData);
+        } catch (e) {
+            alert("Please Check Internet Connection And Try Again.");
+        }
+    }
+});
+
+document.addEventListener('click',function(e) {
+    if (e.target && e.target.id === "showProducts") {
+        let form = document.getElementById("productsForm");
+        form.submit();
+    }
+});
